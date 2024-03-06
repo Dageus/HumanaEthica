7137a4fda499fec625b117660b90015f305b962c
Bernardo Meireles <bernardo.meireles@tecnico.ulisboa.pt>
2024-03-06 19:00:51 +0000

    fix: getVolunteer changed to getVolunteerId, fixes #22

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
@@ -37,7 +37,7 @@ public class ParticipationDto {
         return activityId;
     }
 
-    public UserDto getVolunteer() {
+    public UserDto getVolunteerId() {
         return volunteerId;
     }
 
