a66e760796bd34220226ae25a2822a49c358a537
Joao Melo <joao.melo.correia@tecnico.ulisboa.pt>
2024-03-08 12:07:43 +0000

    feat: create dto for assessment, see #26

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/dto/AssessmentDto.java
@@ -0,0 +1,75 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
+
+public class AssessmentDto {
+    private Integer id;
+    private String review;
+    private String reviewDate;
+    private Integer institution;
+    private Integer user;
+
+    public AssessmentDto() {
+    }
+
+    public AssessmentDto(Assessment assessment) {
+        setId(assessment.getId());
+        setReview(assessment.getReview());
+        setReviewDate(DateHandler.toISOString(assessment.getReviewDate()));
+        setInstitution(assessment.getInstitution().getId());
+        setUser(assessment.getUser().getId());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(String reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public Integer getInstitution() {
+        return this.institution;
+    }
+
+    public void setInstitution(Integer institutionId) {
+        this.institution = institutionId;
+    }
+
+    public Integer getUser() {
+        return this.user;
+    }
+
+    public void setUser(Integer userId) {
+        this.user = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "AssessmentDto{" +
+                "id=" + id +
+                ", review='" + review + '\'' +
+                ", reviewDate='" + reviewDate + '\'' +
+                ", institution=" + institution +
+                ", user=" + user +
+                '}';
+    }
+
+}
