12782b9f98d878557b6e0fa5bb6c435cf015221b
Joao Melo <joao.melo.correia@tecnico.ulisboa.pt>
2024-03-08 12:57:39 +0000

    fix: institution domain include assessment domain, see #31

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/institution/domain/Institution.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/institution/domain/Institution.java
@@ -3,6 +3,7 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain;
 import jakarta.persistence.*;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Member;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
@@ -44,6 +45,9 @@ public class Institution {
 
     @OneToMany(mappedBy = "institution", orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Activity> activities = new ArrayList<>();
+
+    @OneToMany(mappedBy = "institution", orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Assessment> assessments = new ArrayList<>();
 
     public Institution() {
     }
@@ -172,5 +176,21 @@ public class Institution {
         setTokenGenerationDate(DateHandler.now());
         setConfirmationToken(token);
         return token;
+    }
+
+    public void addAssessment(Assessment assessment) {
+        this.assessments.add(assessment);
+    }
+
+    public List<Assessment> getAssessments() {
+        return assessments;
+    }
+
+    public void setAssessments(List<Assessment> assessments) {
+        this.assessments = assessments;
+    }
+
+    public void removeAssessment(Assessment assessment) {
+        this.assessments.remove(assessment);
     }
 }
