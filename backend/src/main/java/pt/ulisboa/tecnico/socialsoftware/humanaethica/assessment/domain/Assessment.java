820234b216aae2330bcfe20d78a3e368cdd26dac
Joao Melo <joao.melo.correia@tecnico.ulisboa.pt>
2024-03-08 12:04:24 +0000

    feat: create domain for assessment, see #25

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/domain/Assessment.java
@@ -0,0 +1,112 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain;
+
+import jakarta.persistence.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity.State;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "assessment")
+public class Assessment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String review;
+
+    private LocalDateTime reviewDate;
+
+    // one instituion can have multiple assessments
+    @ManyToOne
+    @JoinColumn(name = "institution_id")
+    private Institution institution;
+
+    // one user can have multiple assessments
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Volunteer user;
+
+    public Assessment() {
+    }
+
+    public Assessment(Institution institution, Volunteer user, AssessmentDto assessmentDto) {
+        setReview(assessmentDto.getReview());
+        setReviewDate(LocalDateTime.now());
+        setInstitution(institution);
+        setUser(user);
+
+        verifyInvariants();
+    }
+
+    // Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public LocalDateTime getReviewDate() {
+        return reviewDate;
+    }
+
+    public Institution getInstitution() {
+        return institution;
+    }
+
+    public Volunteer getUser() {
+        return user;
+    }
+
+    // Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public void setReviewDate(LocalDateTime reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public void setInstitution(Institution institution) {
+        this.institution = institution;
+        institution.addAssessment(this);
+    }
+
+    public void setUser(Volunteer user) {
+        this.user = user;
+    }
+
+    // Verifications
+    private void verifyInvariants() {
+    }
+
+    // review must have at least 10 characters
+    private void reviewIsRequired() {
+        if (this.review == null || this.review.length() < 10) {
+            throw new HEException(ASSESSMENT_REVIEW_INVALID);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Assessment{" +
+                "id=" + id +
+                ", review='" + review + '\'' +
+                ", reviewDate=" + reviewDate +
+                ", institution=" + institution +
+                ", user=" + user +
+                '}';
+    }
+}
