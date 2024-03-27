fb078fde3dcba2e79b80d60d5c354f2a9ab7ea0d
João Nogueira <joao.miguel.nogueira@tecnico.ulisboa.pt>
2024-03-27 00:24:24 +0000

    feat: added VolunteerName and Participating columns, see #58

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
@@ -6,7 +6,9 @@ import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
 public class EnrollmentDto {
     private Integer id;
     private String motivation;
-    private Integer activityId;
+    private boolean participating;
+    private Integer volunteerId;
+    private String volunteerName;    private Integer activityId;
 
     private String enrollmentDateTime;
 
@@ -16,6 +18,10 @@ public class EnrollmentDto {
     public EnrollmentDto(Enrollment enrollment) {
         this.id = enrollment.getId();
         this.motivation = enrollment.getMotivation();
+        this.participating = enrollment.getActivity().getParticipations().stream().
+            anyMatch(p -> p.getVolunteer().getId().equals(enrollment.getVolunteer().getId()));
+        this.volunteerId = enrollment.getVolunteer().getId();
+        this.volunteerName = enrollment.getVolunteer().getName();
         this.enrollmentDateTime = DateHandler.toISOString(enrollment.getEnrollmentDateTime());
         this.activityId = enrollment.getActivity().getId();
     }
@@ -34,6 +40,30 @@ public class EnrollmentDto {
 
     public void setMotivation(String motivation) {
         this.motivation = motivation;
+    }
+
+    public Integer getVolunteerId() {
+        return volunteerId;
+    }
+
+    public void setVolunteerId(Integer volunteerId) {
+        this.volunteerId = volunteerId;
+    }
+
+    public String getVolunteerName() {
+        return volunteerName;
+    }
+
+    public void setVolunteerName(String volunteerName) {
+        this.volunteerName = volunteerName;
+    }
+
+    public Boolean getParticipating() {
+        return participating;
+    }
+
+    public void setParticipating(Boolean participating) {
+        this.participating = participating;
     }
 
     public String getEnrollmentDateTime() {
--- frontend/src/models/enrollment/Enrollment.ts
+++ frontend/src/models/enrollment/Enrollment.ts
@@ -2,6 +2,9 @@ import { ISOtoString } from '@/services/ConvertDateService';
 
 export default class Enrollment {
   id: number | null = null;
+  participating!: boolean;
+  volunteerId!: number;
+  volunteerName!: string;
   motivation!: string;
   enrollmentDateTime!: string;
   activityId: number | null = null;
@@ -10,6 +13,9 @@ export default class Enrollment {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.motivation = jsonObj.motivation;
+      this.participating = jsonObj.participating;
+      this.volunteerId = jsonObj.volunteerId;
+      this.volunteerName = jsonObj.volunteerName;
       this.enrollmentDateTime = ISOtoString(jsonObj.enrollmentDateTime);
       this.activityId = jsonObj.activityId;
     }
