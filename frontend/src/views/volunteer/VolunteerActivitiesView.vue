f3cf3b1567834895eaf215682d418a2b624b6c53
André Santos <andrefssantos@tecnico.ulisboa.pt>
2024-03-27 19:38:21 +0000

    feat: create button 'Write Assessment' in VolunteerActivitiesView, see #67

--- frontend/src/views/volunteer/VolunteerActivitiesView.vue
+++ frontend/src/views/volunteer/VolunteerActivitiesView.vue
@@ -1,152 +1,221 @@
-<template>
-  <div>
-    <v-card class="table">
-      <v-data-table
-        :headers="headers"
-        :items="activities"
-        :search="search"
-        disable-pagination
-        :hide-default-footer="true"
-        :mobile-breakpoint="0"
-        data-cy="volunteerActivitiesTable"
-      >
-        <template v-slot:top>
-          <v-card-title>
-            <v-text-field
-              v-model="search"
-              append-icon="search"
-              label="Search"
-              class="mx-2"
-            />
-            <v-spacer />
-          </v-card-title>
-        </template>
-        <template v-slot:[`item.themes`]="{ item }">
-          <v-chip v-for="theme in item.themes" v-bind:key="theme.id">
-            {{ theme.completeName }}
-          </v-chip>
-        </template>
-        <template v-slot:[`item.action`]="{ item }">
-          <v-tooltip v-if="item.state === 'APPROVED'" bottom>
-            <template v-slot:activator="{ on }">
-              <v-icon
-                class="mr-2 action-button"
-                color="red"
-                v-on="on"
-                data-cy="reportButton"
-                @click="reportActivity(item)"
-                >warning</v-icon
-              >
-            </template>
-            <span>Report Activity</span>
-          </v-tooltip>
-        </template>
-      </v-data-table>
-    </v-card>
-  </div>
-</template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import RemoteServices from '@/services/RemoteServices';
-import Activity from '@/models/activity/Activity';
-import { show } from 'cli-cursor';
 
-@Component({
-  methods: { show },
-})
-export default class VolunteerActivitiesView extends Vue {
-  activities: Activity[] = [];
-  search: string = '';
-  headers: object = [
-    {
-      text: 'Name',
-      value: 'name',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'Region',
-      value: 'region',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'Participants',
-      value: 'participantsNumberLimit',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'Themes',
-      value: 'themes',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'Description',
-      value: 'description',
-      align: 'left',
-      width: '30%',
-    },
-    {
-      text: 'State',
-      value: 'state',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'Start Date',
-      value: 'formattedStartingDate',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'End Date',
-      value: 'formattedEndingDate',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'Application Deadline',
-      value: 'formattedApplicationDeadline',
-      align: 'left',
-      width: '5%',
-    },
-    {
-      text: 'Actions',
-      value: 'action',
-      align: 'left',
-      sortable: false,
-      width: '5%',
-    },
-  ];
 
-  async created() {
-    await this.$store.dispatch('loading');
-    try {
-      this.activities = await RemoteServices.getActivities();
-    } catch (error) {
-      await this.$store.dispatch('error', error);
-    }
-    await this.$store.dispatch('clearLoading');
-  }
 
-  async reportActivity(activity: Activity) {
-    if (activity.id !== null) {
-      try {
-        const result = await RemoteServices.reportActivity(
-          this.$store.getters.getUser.id,
-          activity.id,
-        );
-        this.activities = this.activities.filter((a) => a.id !== activity.id);
-        this.activities.unshift(result);
-      } catch (error) {
-        await this.$store.dispatch('error', error);
-      }
-    }
-  }
-}
-</script>
 
-<style lang="scss" scoped></style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
