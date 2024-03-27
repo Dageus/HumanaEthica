0871ef199322b323977eabca1081416d882dd507
André Santos <andrefssantos@tecnico.ulisboa.pt>
2024-03-27 19:31:04 +0000

    feat: add volunteerName column to the table InstitutionAssessmentsView, see #72

--- frontend/src/models/assessment/Assessment.ts
+++ frontend/src/models/assessment/Assessment.ts
@@ -4,12 +4,16 @@ export default class Assessment {
   id: number | null = null;
   review!: string;
   reviewDate!: string;
+  institutionId!: number;
+  volunteerName!: string;
 
   constructor(jsonObj?: Assessment) {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.review = jsonObj.review;
       this.reviewDate = ISOtoString(jsonObj.reviewDate);
+      this.institutionId = jsonObj.institutionId;
+      this.volunteerName = jsonObj.volunteerName;
     }
   }
 }
