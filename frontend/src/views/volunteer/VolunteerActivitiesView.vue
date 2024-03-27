e6d606074e9ea8d04c324d4508d1d3ed0d4a253a
Joao Melo <joao.melo.correia@tecnico.ulisboa.pt>
2024-03-27 20:13:54 +0000

    fix: VolunteerActiviesView, see #67
    Commit made by Andre Santos

--- frontend/src/views/volunteer/VolunteerActivitiesView.vue
+++ frontend/src/views/volunteer/VolunteerActivitiesView.vue
@@ -35,10 +35,24 @@
                 v-on="on"
                 data-cy="reportButton"
                 @click="reportActivity(item)"
-                >warning</v-icon
-              >
+                >warning
+              </v-icon>
             </template>
             <span>Report Activity</span>
+          </v-tooltip>
+          <!-- new button -->
+          <v-tooltip v-if="isAssessable(item)" bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon
+                class="mr-2 action-button"
+                color="blue"
+                v-on="on"
+                data-cy="assessmentButton"
+                @click="newAssessment(item)"
+                >mdi-square-edit-outline
+              </v-icon>
+            </template>
+            <span>Write Assessment</span>
           </v-tooltip>
           <v-tooltip v-if="activityAppliable(item)" bottom>
             <template v-slot:activator="{ on }">
@@ -55,12 +69,12 @@
           </v-tooltip>
         </template>
       </v-data-table>
-      <enrollment-dialog
-        v-if="currentActivity && enrollmentDialog"
-        v-model="enrollmentDialog"
+      <assessment-dialog
+        v-if="currentActivity && assessmentDialog"
+        v-model="assessmentDialog"
         :activity="currentActivity"
-        v-on:apply="onApplyActivity"
-        v-on:close-enrollment-dialog="onCloseEnrollmentDialog"
+        v-on:apply="onCreateAssessment"
+        v-on:close-assessment-dialog="onCloseAssessmentDialog"
       />
     </v-card>
   </div>
@@ -70,24 +84,31 @@
 import { Component, Vue } from 'vue-property-decorator';
 import RemoteServices from '@/services/RemoteServices';
 import Activity from '@/models/activity/Activity';
+import AssessmentDialog from '@/views/volunteer/AssessmentDialog.vue';
+import { show } from 'cli-cursor';
+import Participation from '@/models/participation/Participation';
+import Assessment from '@/models/assessment/Assessment';
 import Enrollment from '@/models/enrollment/Enrollment';
 import EnrollmentDialog from '@/views/volunteer/EnrollmentDialog.vue';
-import { show } from 'cli-cursor';
 
 @Component({
   components: {
-    'enrollment-dialog': EnrollmentDialog,
+    'assessment-dialog': AssessmentDialog,
   },
   methods: { show },
 })
 export default class VolunteerActivitiesView extends Vue {
   activities: Activity[] = [];
+  participations: Participation[] = [];
+  assessments: Assessment[] = [];
   enrollments: Enrollment[] = [];
 
+  search: string = '';
+
   currentActivity: Activity | null = null;
+  assessmentDialog: boolean = false;
   enrollmentDialog: boolean = false;
 
-  search: string = '';
   headers: object = [
     {
       text: 'Name',
@@ -156,11 +177,46 @@ export default class VolunteerActivitiesView extends Vue {
     await this.$store.dispatch('loading');
     try {
       this.activities = await RemoteServices.getActivities();
+      this.participations = await RemoteServices.getVolunteerParticipation();
+      this.assessments = await RemoteServices.getVolunteerAssessments();
       this.enrollments = await RemoteServices.getVolunteerEnrollments();
     } catch (error) {
       await this.$store.dispatch('error', error);
     }
     await this.$store.dispatch('clearLoading');
+  }
+
+  async reportActivity(activity: Activity) {
+    if (activity.id !== null) {
+      try {
+        const result = await RemoteServices.reportActivity(
+          this.$store.getters.getUser.id,
+          activity.id,
+        );
+        this.activities = this.activities.filter((a) => a.id !== activity.id);
+        this.activities.unshift(result);
+      } catch (error) {
+        await this.$store.dispatch('error', error);
+      }
+    }
+  }
+
+  //asdasdasd
+  isAssessable(activity: Activity) {
+    // check if activity has ended
+    if (Date.now() < new Date(activity.endingDate).getTime()) return false;
+
+    // check if user has already assessed this institution
+    if (
+      this.assessments.find((a) => a.institutionId === activity.institution.id)
+    )
+      return false;
+
+    // check if user has participated in this activity
+    if (!this.participations.find((p) => p.activityId === activity.id))
+      return false;
+
+    return true;
   }
 
   async reportActivity(activity: Activity) {
@@ -205,6 +261,22 @@ export default class VolunteerActivitiesView extends Vue {
       (enrollment) => enrollment.activityId === activity.id,
     );
     return applicationDeadline >= today && !commonEnrollments;
+  }
+
+  async newAssessment(activity: Activity) {
+    this.currentActivity = activity;
+    this.assessmentDialog = true;
+  }
+
+  onCreateAssessment(assessment: Assessment) {
+    this.assessments.push(assessment);
+    this.currentActivity = null;
+    this.assessmentDialog = false;
+  }
+
+  onCloseAssessmentDialog() {
+    this.currentActivity = null;
+    this.assessmentDialog = false;
   }
 }
 </script>
